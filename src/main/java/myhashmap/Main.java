e90342adc9c4ae023b1258f3df8b6bad658f20fb
kondryukov <80988635+kondryukov@users.noreply.github.com>
2025-10-05 20:21:46 +0300

    Delete src directory

--- src/main/java/myhashmap/Main.java
+++
@@ -1,6 +0,0 @@
-package main.java.myhashmap;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/myhashmap/MyMap.java
+++
@@ -1,8 +0,0 @@
-package main.java.myhashmap;
-
-public interface MyMap<K, V> {
-    V get(K key);
-    void put(K key, V value);
-    V remove(K key);
-    boolean containsKey(K key);
-}
