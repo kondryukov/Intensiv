ec55071f8b89815fced0d40264b9c9b4d2fa8c7a
kondryukov <80988635+kondryukov@users.noreply.github.com>
2025-10-05 16:02:24 +0300

    Update Main.java

--- src/main/java/myhashmap/Main.java
+++ src/main/java/myhashmap/Main.java
@@ -2,8 +2,5 @@ package main.java.myhashmap;
 
 public class Main {
     public static void main(String[] args) {
-        MyMap<String, Integer> map = new MyHashMap<>();
-        map.put(null, 1);
-        System.out.println(map.get(null));
     }
 }
